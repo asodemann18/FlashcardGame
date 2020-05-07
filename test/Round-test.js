cb397adc85508ebce8199f05c0601d5f4dda8f60
Alex Sodemann <asodemann18@gmail.com>
2020-05-07 16:14:23 -0600

    Rename test

--- test/Round-test.js
+++ test/Round-test.js
@@ -27,7 +27,7 @@ describe('Round', function() {
     expect(round.deck).to.deep.equal(deck.cards);
   })
 
-  it('should return the first card in the deck', function() {
+  it('should start with the first card in the deck', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
