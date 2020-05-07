bcfc046a4ce375e1963806ed8d657414cd16b720
Alex Sodemann <asodemann18@gmail.com>
2020-05-07 15:24:44 -0600

    Add test that passes an argument to deck-test.js

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -32,4 +32,13 @@ describe('Deck', function() {
     const cardCounts = deck.countCards();
     expect(cardCounts).to.equal(3);
   });
+
+  it('should return the number of cards when an argument is passed', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const cardCounts = deck.countCards('xyz');
+    expect(cardCounts).to.equal(3);
+  });
 });
